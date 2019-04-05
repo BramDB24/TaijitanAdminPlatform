52b2a88f9b973a1fdd7d0bb4c8518df5c6c456e7
Jonah De Smet <jonah.desmet@hotmail.com>
2019-04-05 12:33:49 +0200

    Klassen toevoegen in domein en packages

---
+++ tests/domeinTesten/DomeinControllerUnitTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domeinTesten;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jonah
+ */
+public class DomeinControllerUnitTest {
+    
+    public DomeinControllerUnitTest() {
+    }
+    
+}
