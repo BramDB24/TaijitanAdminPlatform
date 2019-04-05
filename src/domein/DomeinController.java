912a0533d2646026aca194df899b7d8d4ee162ce
Jonah De Smet <jonah.desmet@hotmail.com>
2019-04-05 13:48:44 +0200

    toevoegen vpproject

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domein;
 
-/**
- *
- * @author Jonah
- */
 public class DomeinController {
-    
+
+	public DomeinController() {
+		// TODO - implement DomeinController.DomeinController
+		throw new UnsupportedOperationException();
+	}
+
 }
