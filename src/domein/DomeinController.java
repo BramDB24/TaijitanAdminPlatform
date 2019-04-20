fc3fe5b9ead315a9b762cd38ea067b221f860e7d
Johanna De Bruycker <johanna.debruycker@student.hogent.be>
2019-04-20 22:11:34 +0200

    Methode aanpassenGebruiker in domeinController

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -2,6 +2,7 @@ package domein;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import repository.GebruikerDao;
 import repository.GebruikerDaoJpa;
@@ -24,6 +25,29 @@ public class DomeinController {
             String stad, String nationaliteit, String emailOuders, String gsm, char geslacht) {
         this.gebruikers.add(new Lid(familienaam, voornaam, wachtwoord, geboortedatum, straat, postcode, land, rijksregisternummer, email, telefoon, geboorteplaats, huisnummer,
                 stad, nationaliteit, emailOuders, gsm, geslacht));
+    }
+    
+    public void aanpassenGebruiker(String gebruikersnaam, String familienaam, String voornaam, String wachtwoord, Date geboortedatum, String straat, int postcode,
+            String land, int rijksregisternummer, String email, String telefoon, String geboorteplaats, int huisnummer,
+            String stad, String nationaliteit, String emailOuders, String gsm, char geslacht) {
+        Gebruiker gebruiker = this.gebruikers.stream().filter(g -> g.getGebruikersNaam().equals(gebruikersnaam)).findFirst().get();
+        gebruiker.setFamilienaam(familienaam);
+        gebruiker.setVoornaam(voornaam);
+        gebruiker.setWachtwoord(wachtwoord);
+        gebruiker.setGeboorteDatum(geboortedatum);
+        gebruiker.setStraatnaam(straat);
+        gebruiker.setPostcode(postcode);
+        gebruiker.setLand(land);
+        gebruiker.setRijksregisternummer(rijksregisternummer);
+        gebruiker.setEmailAdres(email);
+        gebruiker.setVastTelefoonnummer(telefoon);
+        gebruiker.setGeborenTe(geboorteplaats);
+        gebruiker.setHuisnummer(huisnummer);
+        gebruiker.setStad(stad);
+        gebruiker.setNationaliteit(nationaliteit);
+        gebruiker.setEmailAdresOuders(emailOuders);
+        gebruiker.setGsmNummer(gsm);
+        gebruiker.setGeslacht(geslacht);
     }
     
     public void close(){
--- tests/domeinTesten/DomeinControllerUnitTest.java
+++ tests/domeinTesten/DomeinControllerUnitTest.java
@@ -5,9 +5,6 @@
  */
 package domeinTesten;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 /**
  *
  * @author Jonah
