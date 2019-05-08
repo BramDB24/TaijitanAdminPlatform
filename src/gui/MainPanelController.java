edfcbb7752dbe36e65dc62e9d72f6be09e88c54f
Bram De Bleecker <bram.debleecker@student.hogent.be>
2019-05-09 01:06:16 +0200

    Revert "GUI aanpassen"
    This reverts commit 9283bb77fd817debdbe242df97a6e3d796937ac8.

--- src/gui/MainPanelController.java
+++ src/gui/MainPanelController.java
@@ -64,7 +64,6 @@ public class MainPanelController extends GridPane {
 
     @FXML
     public void toonLedenlijst(ActionEvent event) {
-        clearScreen();
         dc = new GebruikerController();
         overzichtPanel = new OverzichtPanelController(dc, this);
         this.add(overzichtPanel, 1, 1);
@@ -75,11 +74,6 @@ public class MainPanelController extends GridPane {
         apc = new AanwezighedenPanelController(dc);
         this.add(apc, 1, 2);
     }*/
-    public void clearScreen()
-    {
-        this.getChildren().removeAll(lidGegevensPanel, parametersPanel,tableOverzichtPanel,opc,overzichtPanel);
-        
-    }
     public void toonItem(Object object) {
         lidGegevensPanel = new LidGegevensPanelController(dc);
         dc.addObserver(lidGegevensPanel);
@@ -89,8 +83,6 @@ public class MainPanelController extends GridPane {
 
     public void toonNogItem(String keuze, OverzichttypesPanelController scherm) {
         OverzichtController<Object> oc = new OverzichtController<>();
-        
-
         if(tableOverzichtPanel == null)
             tableOverzichtPanel = new TableOverzichtPanelController();
         if (!this.getChildren().stream().anyMatch(o -> o instanceof TableOverzichtPanelController)) {
@@ -121,7 +113,6 @@ public class MainPanelController extends GridPane {
 
     @FXML
     private void toonOverzichtenlijst(ActionEvent event) {
-        clearScreen();
         dc = new OverzichtController();
         opc = new OverzichttypesPanelController(dc, this);
         this.add(opc, 1, 0);
