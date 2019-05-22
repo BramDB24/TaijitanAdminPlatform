23c3edfad6ac1f2940b41daa8449feec441dc261
Jonah De Smet <jonah.desmet@hotmail.com>
2019-05-22 23:29:40 +0200

    unittests

--- tests/domeinTesten/GebruikerTest.java
+++ tests/domeinTesten/GebruikerTest.java
@@ -7,9 +7,13 @@ package domeinTesten;
 
 import domein.Gebruiker;
 import domein.Lid;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 /**
  *
@@ -26,27 +30,40 @@ public class GebruikerTest {
 
     public GebruikerTest() {
     }
-    
+
     @Test
-    public void gebruiker_settelefoonnr(){
+    public void gebruiker_settelefoonnr() {
         g.setTelefoonnummer("0478451245");
         Assert.assertEquals("0478451245", g.getTelefoonnummer());
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
-    public void gebruiker_settelefoonnr_throwsexception(){
+    public void gebruiker_settelefoonnr_throwsexception() {
         g.setTelefoonnummer("047");
     }
-    
+
     @Test
-    public void gebruiker_setemail(){
+    public void gebruiker_setemail() {
         g.setEmail("test@test.com");
         Assert.assertEquals("test@test.com", g.getEmail());
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
-    public void gebruiker_setmail_throwsexception(){
+    public void gebruiker_setmail_throwsexception() {
         g.setEmail("dasd");
+    }
+
+    @Test
+    public void gebruiker_setgeboortedatum() {
+        LocalDate date = LocalDate.of(1997, Month.MARCH, 10);
+        g.setGeboortedatum(date);
+        Assert.assertEquals(date, g.getGeboortedatum());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void gebruiker_setgeboortedatum_throwserror() {
+        LocalDate date = LocalDate.of(2020, Month.MARCH, 10);
+        g.setGeboortedatum(date);
     }
 
 }
